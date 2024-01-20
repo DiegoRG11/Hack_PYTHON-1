24e66430df532a6a526d8b6e54eeb590a07d6206
DiegoRG11 <diisaac.36@gmail.com>
2024-01-20 09:25:45 -0400

    Hack de Python completado

---
+++ hack_6.py
@@ -0,0 +1,12 @@
+"""
+loop: for [] output => [0,1,2,3,4,5]
+"""
+
+def fn_hack_6():
+    result = []
+    numbers = [0, 1, 2, 3, 4, 5]
+
+    for x in numbers:
+            result.append(x)
+
+    return result
